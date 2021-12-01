2a5aa2cd5def23ba6b4dc3e4a7f4ee69fac1f241
Aleksandra Bošković <sandraboskovic@hotmail.com>
2021-12-01 04:44:22 +0100

    update/remove mongoose options

--- basicAuth/configs/db.config.js
+++ basicAuth/configs/db.config.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/basicAuth';
 
 mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
+  .connect(MONGODB_URI)
   .then(() => console.log(`Successfully connected to the database ${MONGODB_URI}`))
   .catch(error => {
     console.error(`An error ocurred trying to connect to the database ${MONGODB_URI}: `, error);
