48a85ca78d730e48886611d17b736bc044a3ded6
Marcos Monzón <marcos.monzon.iparraguirre.ext@ing.com>
2023-07-12 20:43:36 +0200

    first commit

--- basicAuth/models/User.model.js
+++ basicAuth/models/User.model.js
@@ -1,25 +1,42 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const SALT_ROUNDS = 10;
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
-      unique: true
+      required: [true, 'Username is required.']
     },
     email: {
       type: String,
       required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
+      unique: [true, 'Email is already registered.'],
+      match: [EMAIL_PATTERN, 'Email is invalid.']
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.']
     }
-    // add password property here
   },
   {
     timestamps: true
   }
 );
+
+userSchema.pre('save', function(next) {
+  if (this.isModified('password')) {
+    bcrypt.hash(this.password, SALT_ROUNDS)
+      .then((hash) => {
+        this.password = hash;
+        next();
+      })
+      .catch(error => next(error));
+  } else {
+    next();
+  }
+});
 
 module.exports = model('User', userSchema);
